748753abdb34ce2d8c0d533f969a3f5e8c144746
Stella Kim <admin@stella.kim>
2020-06-21 16:58:59 -0400

    Add test case for Sulfuras

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -7,6 +7,7 @@ class GildedRoseTest(unittest.TestCase):
     def setUp(self):
         self.item = Item("standard", 1, 1)
         self.aged_brie = Item("Aged Brie", 1, 1)
+        self.sulfuras = Item("Sulfuras, Hand of Ragnaros", 1, 1)
 
     def test_update_quality_decreases_standard_item_sell_in_and_quality_by_1(self):
         gilded_rose = GildedRose([self.item])
@@ -64,6 +65,19 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
 
         self.assertEqual(50, self.aged_brie.quality)
+
+    def test_update_quality_never_decreases_sell_in_or_quality_for_sulfuras(self):
+        gilded_rose = GildedRose([self.sulfuras])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(1, self.sulfuras.sell_in)
+        self.assertEqual(1, self.sulfuras.quality)
+
+        self.sulfuras.sell_in = -1
+
+        self.assertEqual(-1, self.sulfuras.sell_in)
+        self.assertEqual(1, self.sulfuras.quality)
 
 if __name__ == '__main__':
     unittest.main()
