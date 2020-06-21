d79483edea892815d76b1fdcf9de23884a8bfffa
Stella Kim <admin@stella.kim>
2020-06-21 16:55:03 -0400

    Add test coverage for Aged Brie

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,6 +6,7 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
     def setUp(self):
         self.item = Item("standard", 1, 1)
+        self.aged_brie = Item("Aged Brie", 1, 1)
 
     def test_update_quality_decreases_standard_item_sell_in_and_quality_by_1(self):
         gilded_rose = GildedRose([self.item])
@@ -39,6 +40,30 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
 
         self.assertEqual(0, self.item.quality)
+
+    def test_update_quality_increases_quality_for_aged_brie(self):
+        gilded_rose = GildedRose([self.aged_brie])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(2, self.aged_brie.quality)
+
+    def test_update_quality_doubles_quality_increase_for_aged_brie_if_sell_in_is_below_0(self):
+        self.aged_brie.sell_in = -1
+        self.aged_brie.quality = 2
+        gilded_rose = GildedRose([self.aged_brie])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(4, self.aged_brie.quality)
+
+    def test_update_quality_never_increases_quality_over_50(self):
+        self.aged_brie.quality = 50
+        gilded_rose = GildedRose([self.aged_brie])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(50, self.aged_brie.quality)
 
 if __name__ == '__main__':
     unittest.main()
