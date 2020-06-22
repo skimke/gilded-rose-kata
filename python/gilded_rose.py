5ff24c2b622c5bfdc99e074ffcb090ecd6c0f633
Stella Kim <admin@stella.kim>
2020-06-22 11:58:51 -0400

    Refactor backstage passes logic based on sell_in

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -23,24 +23,28 @@ class GildedRose(object):
 
             if "Conjured" in item.name:
                 self.__decrease_quality(item, 2)
+
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in < 6:
-                    self.__increase_quality(item, 3)
-                elif item.sell_in < 11:
-                    self.__increase_quality(item, 2)
-                else:
+                if item.sell_in > 10:
                     self.__increase_quality(item, 1)
+                elif item.sell_in in range(6, 11):
+                    self.__increase_quality(item, 2)
+                elif item.sell_in in range(0, 6):
+                    self.__increase_quality(item, 3)
+                elif item.sell_in < 0:
+                    item.quality = 0
+
             elif item.name == "Aged Brie":
                 self.__increase_quality(item, 1)
+
             else:
                 self.__decrease_quality(item, 1)
 
             item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
                 if "Conjured" in item.name:
                     self.__decrease_quality(item, 2)
-                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0
                 elif item.name == "Aged Brie":
                     self.__increase_quality(item, 1)
                 else:
